c5c9ad72b256e1193608b0021d25683d738b3900
Gourragui_omar <bad.kinner@gmail.com>
2025-12-13 22:40:16 +0100

    stack helper

---
+++ stack_init.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomar <gomar@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/13 00:00:00 by gomar             #+#    #+#             */
+/*   Updated: 2025/12/13 00:00:00 by gomar            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack	*stack_init(void)
+{
+	t_stack	*stack;
+
+	stack = (t_stack *)malloc(sizeof(t_stack));
+	if (!stack)
+		return (NULL);
+	stack->top = NULL;
+	stack->size = 0;
+	return (stack);
+}
+
+t_node	*node_new(int value)
+{
+	t_node	*node;
+
+	node = (t_node *)malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->value = value;
+	node->index = -1;
+	node->next = NULL;
+	node->prev = NULL;
+	return (node);
+}
+
+void	stack_clear(t_stack **stack)
+{
+	t_node	*current;
+	t_node	*next;
+
+	if (!stack || !*stack)
+		return ;
+	current = (*stack)->top;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	free(*stack);
+	*stack = NULL;
+}
---
+++ stack_utils.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomar <gomar@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/13 00:00:00 by gomar             #+#    #+#             */
+/*   Updated: 2025/12/13 00:00:00 by gomar            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	stack_size(t_stack *stack)
+{
+	if (!stack)
+		return (0);
+	return (stack->size);
+}
+
+int	is_sorted(t_stack *stack)
+{
+	t_node	*current;
+
+	if (!stack || !stack->top)
+		return (1);
+	current = stack->top;
+	while (current->next)
+	{
+		if (current->value > current->next->value)
+			return (0);
+		current = current->next;
+	}
+	return (1);
+}
+
+void	stack_push(t_stack *stack, t_node *node)
+{
+	if (!stack || !node)
+		return ;
+	node->next = stack->top;
+	node->prev = NULL;
+	if (stack->top)
+		stack->top->prev = node;
+	stack->top = node;
+	stack->size++;
+}
+
+t_node	*stack_pop(t_stack *stack)
+{
+	t_node	*node;
+
+	if (!stack || !stack->top)
+		return (NULL);
+	node = stack->top;
+	stack->top = node->next;
+	if (stack->top)
+		stack->top->prev = NULL;
+	node->next = NULL;
+	node->prev = NULL;
+	stack->size--;
+	return (node);
+}
